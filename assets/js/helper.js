00853024540c578d2134ce3376d4cc53d4006826
Apple <apple@AppledeMacBook-Air-2.local>
2018-05-12 18:41:38 +0800

    added <i></i> into tag

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -14,7 +14,7 @@ var HTMLcontactGeneric = '<div class="contact-item"><div class="contact-attr">%c
 var HTMLlinkedin = '<div class="contact-item"><a href="https://www.linkedin.com/in/robbie-doyen-619b58b/" target="_blank"><div class="contact-icon-align-box"><i class="ion-social-linkedin contact-icon"></i></div></a><div class="contact-value"><span>%data%</span></div>';
 var HTMLgithub = '<div class="contact-item"><a href="https://github.com/toosmallshoes" target="_blank"><div class="contact-icon-align-box"><i class="ion-social-github contact-icon"></i></div></a><div class="contact-value"><span>%data%</span></div>';
 var HTMLfacebook = '<div class="contact-item"><a href="https://www.facebook.com/robbie.doyen"target="_blank"><div class="contact-icon-align-box"><i class="ion-social-facebook contact-icon"></i></div></a><div class="contact-value"><span>%data%</span></div>';
-var HTMLwebsite = '<div class="contact-item"><a href="https://plus.google.com/111960188261433247359" target="_blank"><div class="contact-icon-align-box"><ion-icon name="googleplus"></ion-icon></div></a><div class="contact-value"><span>%data%</span></div>';
+var HTMLwebsite = '<div class="contact-item"><a href="https://plus.google.com/111960188261433247359" target="_blank"><div class="contact-icon-align-box"><i><ion-icon name="googleplus"></ion-icon><i></div></a><div class="contact-value"><span>%data%</span></div>';
 
     // description
 var HTMLmessageDiv = '<div class="short-description"></div>';
